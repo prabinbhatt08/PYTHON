6c9f9fb802efcd4faeee8bd1d99a255175303105
Prabin Bhatt <bhattsamir2062@gmail.com>
2025-09-13 13:53:47 +0545

    program to find the largest number among three numbers

---
+++ fifth.py
@@ -0,0 +1,13 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+
+if(a>=b and a>=c):
+   print("a is largest",a)
+
+elif(b>=c):
+    print("b is largest",b)
+
+else:
+    print("c is largest",c)    
+    
