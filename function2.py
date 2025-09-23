42bc635add6a5e40555a0c5543b739016ce0f068
Prabin Bhatt <bhattsamir2062@gmail.com>
2025-09-23 21:36:18 +0545

    Program to print length and element of list in single line using function

---
+++ function2.py
@@ -0,0 +1,13 @@
+names = ["Prabin", "Pramananda", "Samir", "Sammy"]
+
+def print_len(list):
+    print(len(list))
+
+print_len(names)
+
+def print_list(list):
+    for item in list:
+        print(item, end=" ")
+
+print_list(names)
+print()
